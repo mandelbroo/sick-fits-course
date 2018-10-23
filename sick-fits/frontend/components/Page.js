4d9ad61ae71f71668358f488f816beecbfefd5ee
Vitalii <vitalii.makarets@gmail.com>
2018-10-23 11:40:23 +0300

    add components and pages

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,66 @@
+import React from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+
+import Header from "../components/Header";
+import Meta from "../components/Meta";
+
+const theme = {
+  red: "#ff0000",
+  black: "#393939",
+  lightgrey: "#e1e1e1",
+  grey: "#3a3a3a",
+  offwhite: "#ededed",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0,0,0, 0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+const Inner = styled.div`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2');
+    font-weight: normal;
+    font-size: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
+export default class Page extends React.Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
+  }
+}
