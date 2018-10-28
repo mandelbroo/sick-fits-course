cfe8efc407b3490176538be2b98855c83a4a224c
Vitalii <vitalii.makarets@gmail.com>
2018-10-28 11:44:54 +0200

    backed items stuff

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  // async items(parent, args, ctx, info) {
+  //   return await ctx.db.query.items();
+  // }
+  items: forwardTo("db")
+};
 
 module.exports = Query;
