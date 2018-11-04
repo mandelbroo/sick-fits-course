2f46f75a432564ef160aa42470a95cf43d138c4f
Vitalii <vitalii.makarets@gmail.com>
2018-11-04 18:38:19 +0200

    Image upload using cloudflare

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -42,8 +42,27 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dxfkueprs/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    );
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
-    const { title, price, description } = this.state;
+    const { title, price, description, image } = this.state;
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error, called, data }) => (
@@ -59,6 +78,18 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {image && <img src={image} alt="Upload preview" width="200" />}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
