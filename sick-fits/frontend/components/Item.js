56435102dba874e8b73b2d14d11df5958bedbc1e
Vitalii <vitalii.makarets@gmail.com>
2018-11-04 18:30:47 +0200

    fix item layout

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -17,26 +17,26 @@ class Item extends Component {
     return (
       <ItemStyles>
         {item.image && <img src={item.image} alt={item.title} />}
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
         <Title>
           <Link href={{ pathName: "/item", query: { id: item.id } }}>
             <a>{item.title}</a>
           </Link>
-          <PriceTag>{formatMoney(item.price)}</PriceTag>
-          <p>{item.description}</p>
-
-          <div className="buttonList">
-            <Link
-              href={{
-                pathname: "update",
-                query: { id: item.id }
-              }}
-            >
-              <a>Edit</a>
-            </Link>
-            <button>Add To Cart</button>
-            <button>Delete</button>
-          </div>
         </Title>
+        <p>{item.description}</p>
+
+        <div className="buttonList">
+          <Link
+            href={{
+              pathname: "update",
+              query: { id: item.id }
+            }}
+          >
+            <a>Edit</a>
+          </Link>
+          <button>Add To Cart</button>
+          <button>Delete</button>
+        </div>
       </ItemStyles>
     );
   }
